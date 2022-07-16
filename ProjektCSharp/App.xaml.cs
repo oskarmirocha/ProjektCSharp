30a8bc2b71bb9a8f3a06d45ae523613534b1739b
Daxu <91376928+oskarmirocha@users.noreply.github.com>
2022-07-17 00:52:33 +0200

    Service

--- ProjektCSharp/App.xaml.cs
+++ ProjektCSharp/App.xaml.cs
@@ -1,10 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Configuration;
-using System.Data;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
 using System.Windows;
+using ProjektCSharp.Data;
 
 namespace ProjektCSharp
 {
@@ -13,5 +10,27 @@ namespace ProjektCSharp
     /// </summary>
     public partial class App : Application
     {
+        private readonly ServiceProvider serviceProvider;
+
+        public App()
+        {
+            ServiceCollection services = new ServiceCollection();
+            IServiceCollection serviceCollection = services.AddDbContext<DbContext>(option =>
+            {
+                option.UseSqlite("Data Source = Film.db");
+                option.UseSqlite("Data Source = Wypożyczenie.db");
+                option.UseSqlite("Data Source = Klient.db");
+                option.UseSqlite("Data Source = Reżyser.db");
+            });
+
+            services.AddSingleton<MainWindow>();
+            serviceProvider = services.BuildServiceProvider();
+        }
+        private void OnStartup(object s, StartupEventArgs e)
+        {
+            var mainWindow = serviceProvider.GetRequiredService<MainWindow>();
+            mainWindow.Show();
+        }
+
     }
 }
